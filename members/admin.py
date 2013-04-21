a71f50789e036a9eb766ae98f8cbd23d37c897f6
timp21337 <timp@paneris.org>
2013-04-21 21:53:46 +0100

    Don't over normalise

--- members/admin.py
+++ members/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from members.models import Member
 
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
+
+class MemberInline(admin.StackedInline):
+    model = Member
+    can_delete = False
+
+
+class UserAdmin(UserAdmin):
+    inlines = (MemberInline, )
+
+
 admin.site.register(Member)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- members/models.py
+++ members/models.py
@@ -1,10 +1,39 @@
+
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+def initialise():
+    me = User.objects.create_superuser('timp', 'timp@paneris.org', 'admin')
+    me.first_name = "Tim"
+    me.last_name = "Pizey"
+    me.save()
 
 
 class Member(models.Model):
     user = models.OneToOneField(User)
 
-    dob = models.DateField()
+    dob = models.DateField(help_text='Format: YYYY/MM/DD',
+                           validators=[MinValueValidator(datetime.date(1900, 7, 22)),
+                                       MaxValueValidator(datetime.date(2012, 12, 12))])
+
+    gender = models.CharField(max_length=1,
+                              choices=(('M', 'Male'), ('F', 'Female')),
+                              default= 'F')
+    role = models.CharField(max_length=1,
+                            choices=(('Member', 'Member'),
+                                     ('Carer', 'Carer'),
+                                     ('Helper', 'Helper'),
+                                     ('Leader', 'Leader'),
+                                     ('Officer', 'Officer'),
+                                     ),
+                            default= 'Member')
+    is_child = models.BooleanField(default=True)
+    mobile_number = models.CharField(max_length=16, blank=True)
+    landline_number = models.CharField(max_length=16, blank=True)
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.user.first_name, self.user.last_name)
