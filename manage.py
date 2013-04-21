bca43650ad22810bd19d650583cadb448aad30d8
timp21337 <timp@paneris.org>
2013-04-21 22:55:58 +0100

    Move import to work with pycharm

--- manage.py
+++ manage.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+from django.core.management import execute_from_command_line
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "membership.settings")
 
-    from django.core.management import execute_from_command_line
 
 # hack to prevent admin prompt
-if  'syncdb' in sys.argv:
+if 'syncdb' in sys.argv:
     sys.argv.append('--noinput')
 
 execute_from_command_line(sys.argv)
