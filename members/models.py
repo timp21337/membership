ccf1df389c2b0ee15294dbd4a1ad092ca1f5160a
timp21337 <timp@paneris.org>
2013-04-24 12:19:51 +0100

    Refactored to allow initialisation

--- members/models.py
+++ members/models.py
@@ -7,18 +7,45 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 def initialise():
-    me = User.objects.create_superuser('timp', 'timp@paneris.org', 'admin')
-    me.first_name = "Tim"
-    me.last_name = "Pizey"
-    me.save()
+    admin = User.objects.create_superuser('admin', 'timp@paneris.org', 'admin')
+    admin.first_name = "Adam"
+    admin.last_name = "Admin"
+    admin.save()
+
+    florap = create_child('Flora', 'Pizey', 'F', '2004-08-22', 'Tim', 'Pizey')
+
+
+def create_child(first_name, family_name, gender, dob,
+                 carer_first_name, carer_family_name, **kwargs):
+    carer_u = User.objects.create_user(username(carer_first_name, carer_family_name))
+    carer_u.first_name = carer_first_name
+    carer_u.family_name = carer_family_name
+    carer_u.save()
+    carer_m = Member()
+    carer_m.user = carer_u
+    carer_m.save()
+
+    child_u = User.objects.create_user(username(first_name, family_name))
+    child_u.first_name = first_name
+    child_u.family_name = family_name
+    child_u.save()
+    child_m = Member()
+    child_m.user = child_u
+    child_m.gender = gender
+    child_m.dob = dob
+    child_m.save()
+    child_c = Child()
+    child_c.member = child_m
+    child_c.carer = carer_m
+    child_c.save()
+
+
+def username(first_name, family_name):
+    return "%s%s)" % (str(first_name).lower(), str(family_name).lower()[0])
 
 
 class Member(models.Model):
     user = models.OneToOneField(User)
-
-    dob = models.DateField(help_text='Format: YYYY/MM/DD',
-                           validators=[MinValueValidator(datetime.date(1900, 7, 22)),
-                                       MaxValueValidator(datetime.date(2012, 12, 12))])
 
     gender = models.CharField(max_length=1,
                               choices=(('M', 'Male'), ('F', 'Female')),
@@ -31,7 +58,7 @@ class Member(models.Model):
                                      ('Officer', 'Officer'),
                                      ),
                             default= 'Member')
-    is_child = models.BooleanField(default=True)
+
     mobile_number = models.CharField(max_length=16, blank=True)
     landline_number = models.CharField(max_length=16, blank=True)
 
@@ -41,12 +68,25 @@ class Member(models.Model):
 
 class Child(models.Model):
     member = models.OneToOneField(Member)
-    carer = models.ForeignKey(Member, related_name='carer')
+    dob = models.DateField(help_text='Format: YYYY/MM/DD',
+                           validators=[MinValueValidator(datetime.date(1900, 7, 22)),
+                                       MaxValueValidator(datetime.date(2012, 12, 12))],
+                           null=True)
+
+
+    carer = models.ForeignKey(Member, related_name='carer', null=True)
     carer_2 = models.ForeignKey(Member, related_name='+', null=True,  on_delete=models.SET_NULL)
-    emergency_contact = models.ForeignKey(Member, related_name='+')
+    emergency_contact = models.ForeignKey(Member, related_name='+', null=True)
+
     allergies = models.TextField()
+    conditions = models.TextField()
+
     doctors_name = models.CharField(max_length=15)
     doctors_phone = models.CharField(max_length=16, blank=True)
+    doctors_address = models.TextField()
 
     class Meta:
         verbose_name_plural = "children"
+
+    def __unicode__(self):
+        return self.member.__unicode__()
--- members/tests.py
+++ members/tests.py
@@ -1,11 +1,15 @@
 from django.test import TestCase
 from members.models import Member
+from members.models import Child
 from datetime import date
 from django.contrib.auth.models import User
 
 
 class MemberModelTest(TestCase):
     def test_creating_a_new_member_and_saving_it_to_the_database(self):
+
+        initial_member_count = Member.objects.all().count()
+        initial_child_count = Child.objects.all().count()
 
         user = User.objects.create_user('bobp', 'bobp@paneris.org', 'password')
         user.first_name = "Bob"
@@ -15,20 +19,32 @@ class MemberModelTest(TestCase):
         member = Member()
         member.user = user
 
-        member.dob = date(2005, 07, 13)
-
         # check we can save it to the database
         member.save()
 
+        child = Child()
+        child.member = member
+        child.dob = date(2005, 07, 13)
+        child.save()
+
         # now check we can find it in the database again
         all_members_in_database = Member.objects.all()
-        self.assertEquals(len(all_members_in_database), 1)
-        only_member_in_database = all_members_in_database[0]
-        self.assertEquals(only_member_in_database, member)
+        self.assertEquals(len(all_members_in_database), initial_member_count + 1)
+        new_member_in_database = all_members_in_database[initial_member_count]
+        self.assertEquals(new_member_in_database, member)
 
-        self.assertEquals(only_member_in_database.user.first_name, "Bob")
-        self.assertEquals(only_member_in_database.dob, member.dob)
+        self.assertEquals(new_member_in_database.user.first_name, "Bob")
+
+        all_children_in_database = Child.objects.all()
+        self.assertEquals(len(all_children_in_database), initial_child_count + 1)
+        new_child_in_database = all_children_in_database[initial_child_count]
+
+        self.assertEquals(new_child_in_database.dob, child.dob)
 
         self.assertEquals('Bob (Pizey)', member.__unicode__())
+        self.assertEquals('Bob (Pizey)', child.__unicode__())
+
+
 
         user.delete()
+
