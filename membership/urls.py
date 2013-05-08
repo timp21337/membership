6c3f8cab1e7d88a4b321a6afa318e9a9563b6806
timp21337 <timp@paneris.org>
2013-05-08 13:14:48 +0100

    Configure admin static urls

--- membership/urls.py
+++ membership/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
