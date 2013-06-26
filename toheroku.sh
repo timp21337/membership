b199898d05db7b713aca5d3fbdc783f6fdb5465b
timp21337 <timp@paneris.org>
2013-06-26 17:23:56 +0100

    Select ruby version

--- toheroku.sh
+++ toheroku.sh
@@ -3,6 +3,10 @@ createdb --user postgres membership
 
 ./manage.py syncdb;
 git push heroku_test master
+
+#set specific version of ruby
+source ~/.rvm/scripts/rvm
+
 heroku db:push --confirm wfolktest postgres://postgres:*@127.0.0.1:5432/membership
 
 ./manage.py load
