20ad79fea3ea7a01082612f282ac8598f073d6bf
timp21337 <timp@paneris.org>
2013-06-26 17:26:28 +0100

    Explain ruby version

--- toheroku.sh
+++ toheroku.sh
@@ -4,7 +4,9 @@ createdb --user postgres membership
 ./manage.py syncdb;
 git push heroku_test master
 
-#set specific version of ruby
+# set specific version of ruby
+# Heroku uses 1.9.2p290 and so must we 
+# or psql date serialisation will fail.
 source ~/.rvm/scripts/rvm
 
 heroku db:push --confirm wfolktest postgres://postgres:*@127.0.0.1:5432/membership
