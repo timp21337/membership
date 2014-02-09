2e1009ba40bca0a817acd2709071ec81039bd06d
timp21337 <timp@paneris.org>
2014-02-09 23:29:42 +0000

    Add title option

--- members/management/commands/output.py
+++ members/management/commands/output.py
@@ -1,10 +1,17 @@
 from django.core.management.base import BaseCommand
 from members.models import Member
-
+from optparse import make_option
 
 class Command(BaseCommand):
-    args = '<selection selection>'
+    option_list = BaseCommand.option_list + (
+        make_option('--title',
+            type="string",
+            dest='title',
+            default='Iffley Fields Woodcraft Elfins Member',
+            help='Title for each page'),
+        )
+    args = '<selectionType> <selectionName>'
     help = 'Create pdf reports'
 
     def handle(self, *args, **options):
-        Member.output(args[0], args[1:])
+        Member.output(args[0], args[1:], **options)
