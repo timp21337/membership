bd4d9a05bacc3549c1be126dc218506996a8bbda
timp21337 <timp@paneris.org>
2014-02-12 16:44:59 +0000

    List diets

---
+++ members/management/commands/diets.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand
+from members.models import Member
+
+
+class Command(BaseCommand):
+    args = '<selection selection>'
+    help = 'Create text report'
+
+    def handle(self, *args, **options):
+      Member.diets(args[0], args[1:])
